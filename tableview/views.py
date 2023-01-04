2fb39ef9f05aa9d3d8453ced3a7dffc169b89655
divaymohan <divaymohan05@gmail.com>
2023-01-04 17:28:41 +0530

    adding table view content

--- tableview/views.py
+++ tableview/views.py
@@ -10,7 +10,53 @@ from django.conf import settings
 class TableView(View):
     # form_class = EmailForm
     template_name = 'tableview/table.html'
-    data = {"name": "this is my data"}
+    data = {
+        "name": "this is my data",
+        "wbs_data": [{
+            "wbs": "S22-2001",
+            "description": "Project kick off",
+            "milestone_due_date": "12/1/2022",
+            "Cap_or_Exp": "Capital",
+            "sow_mso_amount": 50000,
+            "haea_invoice":1234,
+            "haea_invoice_date":"12/1/2022",
+            "status":"Invoiced"
+
+        },
+        {
+            "wbs": "S22-2001",
+            "description": "Project kick off",
+            "milestone_due_date": "12/1/2022",
+            "Cap_or_Exp": "Capital",
+            "sow_mso_amount": 50000,
+            "haea_invoice":1234,
+            "haea_invoice_date":"12/1/2022",
+            "status":"Invoiced"
+
+        },
+        {
+            "wbs": "S22-2001",
+            "description": "Project kick off",
+            "milestone_due_date": "12/1/2022",
+            "Cap_or_Exp": "Capital",
+            "sow_mso_amount": 50000,
+            "haea_invoice":1234,
+            "haea_invoice_date":"12/1/2022",
+            "status":"Invoiced"
+
+        },
+        {
+            "wbs": "S22-2001",
+            "description": "Project kick off",
+            "milestone_due_date": "12/1/2022",
+            "Cap_or_Exp": "Capital",
+            "sow_mso_amount": 50000,
+            "haea_invoice":1234,
+            "haea_invoice_date":"12/1/2022",
+            "status":"Invoiced"
+
+        },]
+    }
 
     def get(self, request, *args, **kwargs):
         return render(request, self.template_name, {'data': self.data})
