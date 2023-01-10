cd653fda91aa16a51b1e1f8851f2c18cf1a68146
Divay Mohan <divaymohan05@gmail.com>
2023-01-10 20:16:11 +0530

    Update settings.py

--- django_email/settings.py
+++ django_email/settings.py
@@ -135,6 +135,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'mohandivay@gmail.com'
-EMAIL_HOST_PASSWORD = 'ctvnfloubmdrjete'
+EMAIL_HOST_PASSWORD = '******'
 EMAIL_USE_TLS = True
 
